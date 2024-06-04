69b920770d9c3c2af8c7ed0f06b84b8439d78c8c
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-05 00:36:06 +0100

    added task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdio.h>
+/**
+ * binary_tree_size - measures the size of a binary tree (number of nodes)
+ * @tree: tree to measure the size of
+ *
+ * Return: size of the tree
+ *         0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t l = 0;
+	size_t r = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+
+	return (l + r + 1);
+}
